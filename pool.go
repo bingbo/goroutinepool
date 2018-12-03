f9273928b121346544fe846887c47cf4083894af
bingbo <ibingbo.zh@gmail.com>
2018-12-03 18:21:14 +0800

    up

---
+++ pool.go
@@ -0,0 +1,150 @@
+/**
+ * @description goroutinepool
+ * @author ibingbo
+ * @date 2018/12/1
+ */
+package goroutinepool
+
+import (
+	"log"
+	"sync"
+	"sync/atomic"
+)
+
+// 任务接口
+type Workable interface {
+	Work()
+}
+
+// 任务结构
+type Worker struct {
+	// 任务动作函数
+	action func()
+}
+
+/**
+ * Work
+ * 任务启动
+ */
+func (worker *Worker) Work() {
+	worker.action()
+}
+
+// 默认最大任务数
+var defaultMaxWorkerCount = 100
+// 默认最大协程数
+var defaultMaxGoroutineCount = 10
+
+// 协程池结构定义
+type GoroutinePool struct {
+	maxGoroutineCount  int            // 最大协程数
+	maxWorkerCount     int            // 最大任务数
+	workerQueue        chan Workable  // 任务队列
+	done               chan struct{}  // 任务状态，是否所有任务已完成
+	waitGroup          sync.WaitGroup // 协程等待控制
+	currentWorkerCount int32          // 当前要执行的任务数
+	once               sync.Once
+}
+
+/**
+ * NewGoroutinePool
+ * 实例化一个协程池
+ */
+func NewGoroutinePool(maxGoroutineCount int, maxWorkerCount int) *GoroutinePool {
+	if maxGoroutineCount == 0 {
+		maxGoroutineCount = defaultMaxGoroutineCount
+	}
+	if maxWorkerCount == 0 {
+		maxWorkerCount = defaultMaxWorkerCount
+	}
+	pool := &GoroutinePool{
+		maxGoroutineCount:  maxGoroutineCount,
+		workerQueue:        make(chan Workable, maxWorkerCount),
+		done:               make(chan struct{}),
+		currentWorkerCount: 0,
+	}
+	return pool
+}
+
+/**
+ * 协程池监控，关闭协程
+ */
+func (pool *GoroutinePool) watch() {
+	for {
+		select {
+		case <-pool.done:
+			pool.shutdown()
+			return
+		default:
+			if atomic.LoadInt32(&pool.currentWorkerCount) == 0 {
+				log.Println("all worker done....")
+				pool.shutdown()
+			}
+		}
+	}
+}
+
+/**
+ * 协程池开始执行
+ */
+func (pool *GoroutinePool) start() {
+	pool.waitGroup.Add(pool.maxGoroutineCount)
+	for i := 0; i < pool.maxGoroutineCount; i++ {
+		go pool.doWork()
+	}
+	go pool.watch()
+}
+
+/**
+ * 提交并执行一个任务
+ */
+func (pool *GoroutinePool) Execute(worker Workable) {
+	pool.workerQueue <- worker
+	atomic.AddInt32(&pool.currentWorkerCount, 1)
+	log.Println("add one worker")
+	pool.once.Do(pool.start)
+}
+
+/**
+ * 开始处理所有的任务
+ */
+func (pool *GoroutinePool) doWork() {
+	for {
+		select {
+		case <-pool.done:
+			log.Println("close goroutine....")
+			pool.waitGroup.Done()
+			pool.shutdown()
+			return
+		case worker := <-pool.workerQueue:
+			if worker != nil {
+				worker.Work()
+				atomic.AddInt32(&pool.currentWorkerCount, -1)
+			}
+		}
+	}
+}
+
+/**
+ * 显式调用手动关闭协程池
+ */
+func (pool *GoroutinePool) Shutdown() {
+	pool.shutdown()
+}
+
+/**
+ * 内部关闭协程操作
+ */
+func (pool *GoroutinePool) shutdown() {
+	pool.done <- struct{}{}
+	pool.once.Do(func() {
+		close(pool.workerQueue)
+	})
+}
+
+/**
+ * 等待所有的任务执行完成
+ */
+func (pool *GoroutinePool) AwaitTermination() {
+	pool.waitGroup.Wait()
+}
---
+++ pool_test.go
@@ -0,0 +1,44 @@
+/**
+ * @description goroutinepool
+ * @author ibingbo
+ * @date 2018/12/1
+ */
+package goroutinepool
+
+import (
+	"log"
+	"testing"
+	"time"
+)
+
+type MyWorker struct {
+}
+
+func (w *MyWorker) Work() {
+	time.Sleep(1 * time.Second)
+	log.Println("*****worker done.....")
+}
+
+func TestPool(t *testing.T) {
+	pool := NewGoroutinePool(10, 30)
+	for i := 0; i < 100; i++ {
+		worker := &MyWorker{}
+		pool.Execute(worker)
+	}
+	pool.AwaitTermination()
+
+}
+
+func TestPool1(t *testing.T) {
+	pool := NewGoroutinePool(10, 30)
+	for i := 0; i < 100; i++ {
+		id := i
+		worker := &Worker{func() {
+			time.Sleep(2 * time.Second)
+			log.Println(id, "worker done...")
+		}}
+		pool.Execute(worker)
+	}
+	pool.AwaitTermination()
+
+}
